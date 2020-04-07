2ef24ba9eaff551e3ad7f9716aebe8c4520523af
TheDiamondDoge <supportonly@yandex.ru>
2020-04-07 16:32:35 +0300

    autodetect slide content overflow

--- html-to-ppt/src/main/java/com/company/Main.java
+++ html-to-ppt/src/main/java/com/company/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\TheDiamondDoge\\IdeaProjects\\html-to-ppt\\src\\main\\resources\\file.html";
+        String path = "C:\\Users\\TheDiamondDoge\\IdeaProjects\\management_board_file_processors\\html-to-ppt\\src\\main\\resources\\file.html";
         new HtmlExtractor(path).extract();
     }
 }
--- html-to-ppt/src/main/java/com/company/PptCreator.java
+++ html-to-ppt/src/main/java/com/company/PptCreator.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import org.apache.poi.sl.usermodel.AutoNumberingScheme;
 import org.apache.poi.xslf.usermodel.*;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
 
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,40 +16,84 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PptCreator {
+    private final int MAX_ROW_WIDTH = 626;
+    private final int MAX_ROW_IN_SLIDE = 18;
+    private final int FONT_SIZE = 14;
+    private final String FONT_NAME = "Calibri";
+    private final String SAVE_PATH = "TextFormat.pptx";
+
     private XMLSlideShow ppt;
     private XSLFSlideMaster slideMaster;
     private XSLFSlide currentSlide;
     private XSLFTextShape currentBody;
     private XSLFTextParagraph currentParagraph;
 
+    private boolean NUMERIC_BULLETS = false;
+    private int rowsInCurrentSlide = 0;
+    private int currentRowWidth = 0;
+
     public PptCreator() {
         this.ppt = new XMLSlideShow();
         this.slideMaster = ppt.getSlideMasters().get(0);
+        addSlideWithParagraph();
+//        this.currentBody = currentSlide.getPlaceholder(1);
+    }
+
+    private void addSlideWithParagraph() {
         addEmptySlide();
-        this.currentBody = currentSlide.getPlaceholder(1);
+        currentBody = currentSlide.getPlaceholder(1);
+        rowsInCurrentSlide -= MAX_ROW_IN_SLIDE;
+    }
+
+    private void addEmptySlide() {
+        XSLFSlideLayout layout = slideMaster.getLayout(SlideLayout.TITLE_AND_CONTENT);
+        currentSlide = ppt.createSlide(layout);
+
+        XSLFTextShape header = currentSlide.getPlaceholder(0);
+        XSLFTextShape body = currentSlide.getPlaceholder(1);
+
+        body.clearText();
     }
 
     public void createNewParagraph(boolean bullets) {
         createParagraph();
         currentParagraph.setBullet(bullets);
-    }
 
-    public void addLineBreak() {
-        currentParagraph.addLineBreak();
-    }
+        if (bullets && NUMERIC_BULLETS) {
+            currentParagraph.setBulletAutoNumber(AutoNumberingScheme.arabicPlain, 1);
+        }
 
-    public void addNodeToSlide(Node node, Element element) {
-        TextNode textNode = (TextNode) node;
-        XSLFTextRun textRun = getTextRun(element);
-        textRun.setText(textNode.text());
+        rowsInCurrentSlide += 1;
     }
 
     private void createParagraph() {
         currentParagraph = currentBody.addNewTextParagraph();
     }
 
+    public void addNodeToSlide(Node node, Element element) {
+        setParagraphParams(element);
+        TextNode textNode = (TextNode) node;
+        XSLFTextRun textRun = getTextRun(element);
+
+        increaseCharsAmount(textNode.text());
+        if (isOverflowIfExists()) {
+            addSlideWithParagraph();
+//            rowsInCurrentSlide -= MAX_ROW_IN_SLIDE;
+        }
+
+        textRun.setText(textNode.text());
+    }
+
+    private void setParagraphParams(Element element) {
+        if (element.tagName().equals("ol")) {
+            NUMERIC_BULLETS = true;
+        } else if (element.tagName().equals("ul")) {
+            NUMERIC_BULLETS = false;
+        }
+    }
+
     private XSLFTextRun getTextRun(Element e) {
-        XSLFTextRun textRun = currentParagraph.addNewTextRun();
+        XSLFTextRun textRun = getDefaultTextRun();
         String tag = e.tagName();
         String style = e.attr("style");
         String[] styleAttrs = style.split(";");
@@ -56,6 +103,38 @@ public class PptCreator {
             addDecorationByStyle(textRun, attr);
         }
 
+        return textRun;
+    }
+
+    private void increaseCharsAmount(String text) {
+        AffineTransform affineTransform = new AffineTransform();
+        FontRenderContext frc = new FontRenderContext(affineTransform, true, true);
+        Font font = new Font(FONT_NAME, Font.PLAIN, FONT_SIZE);
+//        System.out.println(font.getStringBounds(text, frc).getWidth());
+
+        currentRowWidth += (int) font.getStringBounds(text, frc).getWidth();
+        if (currentRowWidth >= MAX_ROW_WIDTH) {
+            int fullRows = currentRowWidth / MAX_ROW_WIDTH;
+            rowsInCurrentSlide += fullRows;
+            currentRowWidth = currentRowWidth % MAX_ROW_WIDTH;
+        }
+    }
+
+    private boolean isOverflowIfExists() {
+        return rowsInCurrentSlide > MAX_ROW_IN_SLIDE;
+    }
+
+    public void save() throws IOException {
+        File file = new File(SAVE_PATH);
+        FileOutputStream out = new FileOutputStream(file);
+        ppt.write(out);
+        out.close();
+    }
+
+    private XSLFTextRun getDefaultTextRun() {
+        XSLFTextRun textRun = currentParagraph.addNewTextRun();
+        textRun.setFontFamily(FONT_NAME);
+        textRun.setFontSize((double) FONT_SIZE);
         return textRun;
     }
 
@@ -99,20 +178,5 @@ public class PptCreator {
                 run.setStrikethrough(true);
                 break;
         }
-    }
-
-    private void addEmptySlide() {
-        XSLFSlideLayout layout = slideMaster.getLayout(SlideLayout.TITLE_AND_CONTENT);
-        currentSlide = ppt.createSlide(layout);
-
-        XSLFTextShape body = currentSlide.getPlaceholder(1);
-        body.clearText();
-    }
-
-    public void save() throws IOException {
-        File file = new File("TextFormat.pptx");
-        FileOutputStream out = new FileOutputStream(file);
-        ppt.write(out);
-        out.close();
     }
 }
